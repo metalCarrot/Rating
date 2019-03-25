05134a96acc7ddd0d8dc0f438a4e76a49dc5de92
darkwoolf <woolftreuser@gmail.com>
2019-03-25 18:08:03 +0200

    finish doing sort functions

--- index.py
+++ index.py
@@ -1,9 +1,45 @@
 import json
+import csv
 from pprint import pprint
+from operator import itemgetter, attrgetter
 
-with open('rating.json') as f:
-    data = json.load(f)
+def getAbit():
+    with open('rating.json') as f:
+        data = json.load(f)
 
-for key in data:
-	marks = list(key.get('subjects').values())
-	pprint((int(marks[0]) + int(marks[1]) + int(marks[2])) // 3)
+    students = []
+
+    for key in data:
+        points = list(key.get('subjects').values())
+        marks = []
+        for word in points:
+            marks.append(int(word))
+
+        avg = sum(marks) // len(marks)
+        abit = [key.get('name'), key.get('surname'), avg]
+        students.append(abit)
+
+    return students    
+
+
+def addPlace(students):
+    for (index, student) in enumerate(students):
+        student.insert(0, index + 1)
+    
+    return students
+
+
+def sortAbit(students):
+    students = sorted(students, key=itemgetter(2))
+    students.reverse()
+    students = addPlace(students)
+    return students
+
+
+def printInCsv():
+    pass
+
+
+students = getAbit()
+sortStudent = sortAbit(students)
+pprint(sortStudent)
